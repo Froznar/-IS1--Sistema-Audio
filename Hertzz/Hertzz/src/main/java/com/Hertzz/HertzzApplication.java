dd0e202f29938a5c8df0c9d8ffdeee754e152344
Froznar <francisco.marin@ucsp.edu.pe>
2016-06-14 14:36:11 -0500

    Cambios Base

--- Hertzz/Hertzz/src/main/java/com/Hertzz/HertzzApplication.java
+++ Hertzz/Hertzz/src/main/java/com/Hertzz/HertzzApplication.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
 @Controller
@@ -15,6 +19,7 @@ public class HertzzApplication {
 		SpringApplication.run(HertzzApplication.class, args);
 	}
 	
+		
 	@RequestMapping("/Hertzz")
 	@ResponseBody
 	public void login(String opcion){
