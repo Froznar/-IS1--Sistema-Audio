21b86e61e60a696e615cacc4186cf5720562e08e
paulacamargo25 <paulitacv25@gmail.com>
2016-07-09 13:16:10 -0500

    Add files via upload

---
+++ Hertzz/Hertzz/src/test/java/com/Hertzz/HertzzApplication.java
@@ -0,0 +1,116 @@
+package com.Hertzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.Hertzz.dominio.Album;
+import com.Hertzz.dominio.Artista;
+import com.Hertzz.dominio.Cancion;
+import com.Hertzz.dominio.Playlist;
+import com.Hertzz.dominio.Usuario;
+import com.Hertzz.repositorio.CancionRepositorio;
+import com.Hertzz.repositorio.UsuarioRepositorio;
+
+@EnableAutoConfiguration
+@Controller
+public class HertzzApplication {
+	
+	@Autowired
+	UsuarioRepositorio usuarioRepositorio;
+	
+	@Autowired
+	CancionRepositorio cancionRepositorio;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HertzzApplication.class, args);
+	}
+	
+		
+	@RequestMapping("/Hertzz")
+	@ResponseBody
+	public void login(String opcion){
+		
+	}
+	
+	@RequestMapping("/usuarios")
+	@ResponseBody
+	public List<Usuario> usuarios(){
+		return usuarioRepositorio.findAll();
+	}
+	
+	@RequestMapping("/usuario")
+	@ResponseBody
+	public Usuario usuario(String usuario){
+		return usuarioRepositorio.find_usuario(usuario);
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	@ResponseBody
+	public String login(String usuario, String contrasenha){
+		Usuario user = usuarioRepositorio.login_usuario(usuario, contrasenha);
+		if(user != null) return "true";
+		return "false";
+	}
+	
+	@RequestMapping(value = "/addUsuario", method = RequestMethod.POST)
+	@ResponseBody
+	public String addUsuario(@RequestBody Usuario usuario){
+		Usuario user = usuarioRepositorio.find_usuario(usuario.Nombre_usuario);
+		if(user!=null) return "false";
+
+		usuarioRepositorio.save(usuario);
+		return "true";
+	}
+	
+	@RequestMapping(value = "/canciones", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Cancion> canciones(String cancion){
+		return usuarioRepositorio.Buscar_Cancion(cancion);
+	}
+	
+	@RequestMapping(value = "/albumes", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Album> albumes(String album){
+		return usuarioRepositorio.Buscar_Album(album);
+	}
+	
+
+	@RequestMapping(value = "/top100Canciones", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Cancion> top100(){
+		return cancionRepositorio.Top100();
+	}
+	
+	@RequestMapping(value = "/top10Usuarios", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Usuario> top10(){
+		return usuarioRepositorio.Top10();
+	}
+
+	
+	@RequestMapping(value = "/search_usuario", method = RequestMethod.GET)
+	@ResponseBody
+	public Usuario search_usuario(String usuario){
+		Usuario user = usuarioRepositorio.find_usuario(usuario);
+		return user;
+	}
+	
+	@RequestMapping("/seguidores")
+	@ResponseBody
+	public List<Usuario> seguidores(String usuario){
+	return usuarioRepositorio.find_seguidores_id(usuario);
+	}
+}
