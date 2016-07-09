baeef4f3d96770ac74e0919ed1cb499174ed12ec
Rapange <h_ero@hotmail.es>
2016-07-08 21:38:47 -0500

    Buscar Cancion y Album

--- Hertzz/Hertzz/src/main/java/com/Hertzz/dominio/Album.java
+++ Hertzz/Hertzz/src/main/java/com/Hertzz/dominio/Album.java
@@ -19,10 +19,13 @@ public class Album {
 	@SequenceGenerator(name = "Album_ID_GENERATOR", sequenceName = "Album_ID_SEQ")
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Album_ID_GENERATOR")
 	private Integer Album_id;
-	private String Nombre_Album;
+	public String Nombre_Album;
 	@OneToMany(mappedBy = "Album_id")
 	private List<Cancion> Canciones_Album;
 	private String Fecha_Album;
+	
+	public Album(){
+	}
 	public Album(String nombre){
 		this.Nombre_Album = nombre;
 		this.Fecha_Album = new SimpleDateFormat("MM/dd/yyyy").format(Calendar.getInstance().getTime());
